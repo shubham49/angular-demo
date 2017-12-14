63e7287d5904585cee33c1f155567d7d0d5475c3
shubham49 <gargshubham49@gmail.com>
2017-12-14 10:34:06 +0530

    cross platform check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: Http) { }
+
+  onClick() {
+    const headers: Headers = new Headers({ 'content-Type': 'application/json' });
+    const option = new RequestOptions({ headers: headers });
+    this.http.get('https://httpbin.org/get')
+      .subscribe(
+      data => {
+        if (data) {
+          console.log(data);
+        }
+      },
+      err => {
+        console.log("Error occured.")
+      }
+      );
+  }
 }
